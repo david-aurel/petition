50c04f05d3e63259281adc274b764540225a5746
David Durlan <david.a.durlan@gmail.com>
2020-01-10 22:21:15 +0100

    refactor canvas script

--- public/script.js
+++ public/script.js
@@ -3,29 +3,24 @@ let c = $('canvas'),
     canvas = document.querySelector('canvas'),
     ctx = canvas.getContext('2d');
 
-c.mouseenter(() => {
-    c.mousedown(e => {
-        let x = e.pageX - e.target.offsetLeft,
-            y = e.pageY - e.target.offsetTop;
+c.mousedown(e => {
+    let x = e.pageX - e.target.offsetLeft,
+        y = e.pageY - e.target.offsetTop;
 
-        c.mousemove(e => {
-            let newX = e.pageX - e.target.offsetLeft,
-                newY = e.pageY - e.target.offsetTop;
-            ctx.beginPath();
-            ctx.moveTo(x, y);
-            ctx.lineTo(newX, newY);
-            ctx.stroke();
-            (x = newX), (y = newY);
-        });
+    c.mousemove(e => {
+        let newX = e.pageX - e.target.offsetLeft,
+            newY = e.pageY - e.target.offsetTop;
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+        ctx.lineTo(newX, newY);
+        ctx.stroke();
+        (x = newX), (y = newY);
     });
-    c.mouseup(() => {
-        var dataURL = canvas.toDataURL();
-        sig.val(dataURL);
-        console.log(sig.val());
+});
+c.mouseup(() => {
+    var dataURL = canvas.toDataURL();
+    sig.val(dataURL);
+    console.log(sig.val());
 
-        c.unbind('mousemove');
-    });
-    c.mouseleave(() => {
-        c.unbind('mousemove');
-    });
+    c.unbind('mousemove');
 });
