7fb685d90e219bd3b2b1fd8537f5865b64651b63
David Durlan <david.a.durlan@gmail.com>
2020-01-13 12:04:51 +0100

    update tables to include registering a user

--- signatures.sql
+++ signatures.sql
@@ -3,9 +3,10 @@ DROP TABLE IF EXISTS signatures;
 CREATE TABLE signatures
 (
     id SERIAL PRIMARY KEY,
+    userId INT NOT NULL REFERENCES users(id),
     first VARCHAR NOT NULL CHECK (first != ''),
     last VARCHAR NOT NULL CHECK (last != ''),
-    msg VARCHAR NOT NULL,
+    msg VARCHAR,
     sig VARCHAR NOT NULL CHECK (sig != ''),
-    time VARCHAR
+    time VARCHAR NOT NULL
 );
