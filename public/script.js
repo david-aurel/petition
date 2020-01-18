a0a5849431b6e9e540fa76c608caefc2ee6ddd3f
David Durlan <david.a.durlan@gmail.com>
2020-01-18 22:52:35 +0100

    prevent scrolling when signing on mobile

--- public/script.js
+++ public/script.js
@@ -43,12 +43,14 @@ c.mouseleave(() => {
 
 // draw on mobile
 canvas.ontouchstart = e => {
+    e.preventDefault();
     ctx.strokeStyle = '#0275d8';
     x = e.touches[0].clientX - rect.left;
     y = e.touches[0].clientY - rect.top;
 };
 
 canvas.ontouchmove = e => {
+    e.preventDefault();
     let newX = e.touches[0].clientX - rect.left,
         newY = e.touches[0].clientY - rect.top;
     ctx.beginPath();
